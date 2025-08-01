46a4e78597b6e4900ead3d420d8e172f75c0077c
Davi Chagas <davih.chagas108@gmail.com>
2025-08-01 00:38:26 -0300

    creating the timer

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,30 +1,32 @@
-import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
-import { Cycles } from "../Cycles";
-import { DefaultButton } from "../DefaultButton";
-import { DefaultInput } from "../DefaultInput";
-import { useRef } from "react";
-import type { TaskModel } from "../../models/TaskModel";
-import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
-import { getNextCycle } from "../../utils/getNextCycle";
-import { getNextCycleType } from "../../utils/getNextCycleType";
-import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { DefaultInput } from '../DefaultInput';
+import { useRef } from 'react';
+import { type TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
+import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
+import { Tips } from '../Tips';
 
 export function MainForm() {
-  const { state, setState } = useTaskContext();
+  const { state, dispatch } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
+  // ciclos
   const nextCycle = getNextCycle(state.currentCycle);
-  const nextCycleType = getNextCycleType(nextCycle);
+  const nextCyleType = getNextCycleType(nextCycle);
 
-  function handleCreateNewTask(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
     if (taskNameInput.current === null) return;
 
     const taskName = taskNameInput.current.value.trim();
 
     if (!taskName) {
-      alert("Digite o nome da tarefa");
+      alert('Digite o nome da tarefa');
       return;
     }
 
@@ -34,79 +36,75 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: state.config[nextCycleType],
-      type: nextCycleType,
+      duration: state.config[nextCyleType],
+      type: nextCyleType,
     };
 
-    const secondsRemaining = newTask.duration * 60;
+    dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
 
-    setState((prevState) => {
-      return {
-        ...prevState,
-        config: { ...prevState.config },
-        activeTask: newTask,
-        currentCycle: nextCycle,
-        secondsRemaining,
-        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
-        tasks: [...prevState.tasks, newTask],
-      };
-    });
+    const worker = new Worker(
+      new URL('../../workers/timerWorker.js', import.meta.url),
+    );
+
+    worker.postMessage('FAVOR'); // Sim, posso fazer um favor
+    worker.postMessage('FALA_OI'); // OK: OI!
+    worker.postMessage('BLALBLA'); // Não entendi!
+    worker.postMessage('FECHAR'); // Tá bom, vou fechar
+
+    worker.onmessage = function (event) {
+      console.log('PRINCIPAL recebeu:', event.data);
+    };
   }
 
   function handleInterruptTask() {
-    setState((prevState) => {
-      return {
-        ...prevState,
-        activeTask: null,
-        currentCycle: nextCycle,
-        secondsRemaining: 0,
-        formattedSecondsRemaining: "00:00",
-      };
-    });
+    dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 
   return (
-    <form className="form" onSubmit={handleCreateNewTask}>
-      <div className="formRow">
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
+      <div className='formRow'>
         <DefaultInput
-          labelText="Task:"
-          type="text"
-          id="meuInput"
-          placeholder="Digite Algo"
+          labelText='task'
+          id='meuInput'
+          type='text'
+          placeholder='Digite algo'
           ref={taskNameInput}
           disabled={!!state.activeTask}
         />
       </div>
 
-      <div className="formRow">
-        <p>Próximo intervalo é de 25 min</p>
+      <div className='formRow'>
+        <Tips />
       </div>
 
       {state.currentCycle > 0 && (
-        <div className="formRow">
+        <div className='formRow'>
           <Cycles />
         </div>
       )}
 
-      <div className="formRow">
-        {!state.activeTask ? (
+      <div className='formRow'>
+        {!state.activeTask && (
           <DefaultButton
-            aria-label="Iniciar nova tarefa"
-            title="Iniciar uma nova tarefa"
-            type="submit"
+            aria-label='Iniciar nova tarefa'
+            title='Iniciar nova tarefa'
+            type='submit'
             icon={<PlayCircleIcon />}
-            color="green"
-            key='Este é o botão de submit'
+            key='botao_submit'
           />
-        ) : (<DefaultButton
-            aria-label="Interromper tarefa atual"
-            title="Interromper tarefa atual"
-            type="button"
-            icon={<StopCircleIcon/>}
-            color="red"
+        )}
+
+        {!!state.activeTask && (
+          <DefaultButton
+            aria-label='Interromper tarefa atual'
+            title='Interromper tarefa atual'
+            type='button'
+            color='red'
+            icon={<StopCircleIcon />}
             onClick={handleInterruptTask}
-            key='Este não é o botão de submit'
-          />)}
+            key='botao_button'
+          />
+        )}
       </div>
     </form>
   );
--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,16 +1,49 @@
-import { useState } from "react";
-import { initialTaskState } from "./initialTaskState";
-import { TaskContext } from "./TaskContext";
+import { useEffect, useReducer } from 'react';
+import { initialTaskState } from './initialTaskState';
+import { TaskContext } from './TaskContext';
+import { taskReducer } from './taskReducer';
+import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
+import { TaskActionTypes } from './taskActions';
 
 type TaskContextProviderProps = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
-export function TaskContextProvider({children} : TaskContextProviderProps) {
-    const [state, setState] = useState(initialTaskState);
+export function TaskContextProvider({ children }: TaskContextProviderProps) {
+  const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage(e => {
+    const countDownSeconds = e.data;
+
+    if (countDownSeconds <= 0) {
+      dispatch({
+        type: TaskActionTypes.COMPLETE_TASK,
+      });
+      worker.terminate();
+    } else {
+      dispatch({
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSeconds },
+      });
+    }
+  });
+
+  useEffect(() => {
+    console.log(state);
+
+    if (!state.activeTask) {
+      console.log('Worker terminado por falta de activeTask');
+      worker.terminate();
+    }
+
+    worker.postMessage(state);
+  }, [worker, state]);
+
   return (
-      <TaskContext.Provider value={{state, setState}}>
-        {children}
-      </TaskContext.Provider>
-  )
+    <TaskContext.Provider value={{ state, dispatch }}>
+      {children}
+    </TaskContext.Provider>
+  );
 }
